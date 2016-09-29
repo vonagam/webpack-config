3604fcd13912957f72da92cd5a8d713e86dda2be
Dmitry Maganov <vonagam@gmail.com>
2016-09-29 12:21:22 +0300

    move output.library to output

--- config/parts/environment.production.js
+++ config/parts/environment.production.js
@@ -14,21 +14,6 @@ module.exports = function ( config, options ) {
   config.output.publicPath = options.get( 'production.output.publicPath', '' );
 
 
-  var library = options.get( 'production.output.library' );
-
-  if ( library == true ) library = '[name]';
-
-  if ( library ) {
-
-    config.output.library = library;
-
-    config.output.libraryTarget = options.get( 'production.output.libraryTarget', 'umd' );
-
-    config.output.umdNamedDefine = options.get( 'production.output.umdNamedDefine', true );
-
-  }
-
-
   config.plugins.push( new webpack.DefinePlugin( { 'process.env.NODE_ENV': '"production"' } ) );
 
 };
--- config/parts/output.js
+++ config/parts/output.js
@@ -7,4 +7,19 @@ module.exports = function ( config, options ) {
 
   config.output.sourcePrefix = options.get( 'output.sourcePrefix', '' );
 
+
+  var library = options.get( 'output.library', false );
+
+  if ( library == true ) library = '[name]';
+
+  if ( library ) {
+
+    config.output.library = library;
+
+    config.output.libraryTarget = options.get( 'output.libraryTarget', 'umd' );
+
+    config.output.umdNamedDefine = options.get( 'output.umdNamedDefine', true );
+
+  }
+
 };
